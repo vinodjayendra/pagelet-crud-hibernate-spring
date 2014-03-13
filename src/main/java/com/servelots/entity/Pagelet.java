69be88b4595f11770c549d204cca34fc60c65970
vjayendra <vjayendra@paypal.com>
2014-03-13 15:20:13 -0400

    Updated Pagelet object with title property.

--- src/main/java/com/servelots/entity/Pagelet.java
+++ src/main/java/com/servelots/entity/Pagelet.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 public class Pagelet {
     private String id;
 
+    private String title;
+
     @Id
     public String getId() {
         return id;
@@ -16,5 +18,14 @@ public class Pagelet {
 
     public void setId(String id) {
         this.id = id;
+    }
+
+    @Column
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
